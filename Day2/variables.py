01d0367070cbe879e84427b49d4b70196e40962c
adrianorszulik <orszulik.adrian@gmail.com>
2024-09-21 20:14:38 +0200

    variables day2

---
+++ Day2/variables.py
@@ -0,0 +1,11 @@
+# name = "John"
+# print(name)
+#
+# name  = "Jane"
+# print(name)
+#
+# name = input("What's your name? ")
+# print(name)
+
+course = "Python"
+print(f"You are taking a {course} course")

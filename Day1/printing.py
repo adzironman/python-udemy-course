883db374fd9b8e0db66161a2484706dd36265e61
adrianorszulik <orszulik.adrian@gmail.com>
2024-09-21 20:17:39 +0200

    Day1 completed

---
+++ Day1/printing.py
@@ -0,0 +1,8 @@
+# print("First Print")
+# print(10 +142)
+#
+# print(input("What are you learning today?"))
+#
+# print(input("What's your name?") + input("What's your surname?"))
+
+print("The name of your beer is: '" + input("What's your name?: ") + " " + input("What's your dog's name? ") + "'")
